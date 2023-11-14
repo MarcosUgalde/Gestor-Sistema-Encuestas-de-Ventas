b182b36f4203f22cadbc2262015e473de7792d26
Marcos <marcosugaldemartinez@gmail.com>
2023-11-14 12:28:14 +0100

    feat: creada tabla de usuarios

---
+++ backend-encuestas/sql/creation.sql
@@ -0,0 +1,9 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE IF NOT EXISTS users (
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    email TEXT NOT NULL UNIQUE,
+    username TEXT NOT NULL,
+    password TEXT NOT NULL,
+    edit_access BOOLEAN DEFAULT FALSE
+);
