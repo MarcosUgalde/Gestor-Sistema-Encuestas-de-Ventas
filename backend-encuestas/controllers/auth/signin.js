3943b0e1935110a816eb54183595c55a4b36d982
Marcos <marcosugaldemartinez@gmail.com>
2023-11-14 14:03:45 +0100

    fix: se arregla un error de código en el controlador del login

--- backend-encuestas/controllers/auth/signin.js
+++ backend-encuestas/controllers/auth/signin.js
@@ -1,11 +1,11 @@
 const { hash, serialize } = require("simple-stateless-auth-library");
 const errors = require("../../misc/errors");
-const { selectUser } = require("../../models/auth");
+const { getUser } = require("../../models/auth");
 
 module.exports = (db) => async (req, res, next) => {
   const { email, password } = req.body;
 
-  const response = await selectUser(await db)(email, hash.compare(password));
+  const response = await getUser(await db)(email, hash.compare(password));
 
   if (!response.ok) return next(errors[response.error_code || 500]);
 
