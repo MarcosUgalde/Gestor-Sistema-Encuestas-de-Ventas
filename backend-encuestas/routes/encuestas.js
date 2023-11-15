2aa54d29e72d92748c71dbe7bc50273832d48d84
Marcos <marcosugaldemartinez@gmail.com>
2023-11-15 14:06:17 +0100

    feat: se crea con éxito la ruta para la obtención de las encuestas almacenadas

--- backend-encuestas/routes/encuestas.js
+++ backend-encuestas/routes/encuestas.js
@@ -4,6 +4,7 @@ const { authorizer } = require("../middlewares");
 const encuestasControllers = require("../controllers/encuestas");
 
 module.exports = (db) => {
+  router.get("/all", authorizer(), encuestasControllers.getEncuestas(db));
   router.post("/new", authorizer(), encuestasControllers.addEncuesta(db));
 
   return router;
