120083c6dc04cf48bdaacb052c3fc21f5c639570
Marcos <marcosugaldemartinez@gmail.com>
2023-11-15 14:44:25 +0100

    feat: se crea con éxito la ruta para editar encuestas

--- backend-encuestas/routes/encuestas.js
+++ backend-encuestas/routes/encuestas.js
@@ -4,9 +4,10 @@ const { authorizer } = require("../middlewares");
 const encuestasControllers = require("../controllers/encuestas");
 
 module.exports = (db) => {
+  router.post("/new", authorizer(), encuestasControllers.addEncuesta(db));
   router.get("/all", authorizer(), encuestasControllers.getEncuestas(db));
   router.get("/:id", authorizer(), encuestasControllers.getOneEncuesta(db));
-  router.post("/new", authorizer(), encuestasControllers.addEncuesta(db));
+  router.put("/:id", authorizer(), encuestasControllers.editEncuesta(db));
 
   return router;
 };
