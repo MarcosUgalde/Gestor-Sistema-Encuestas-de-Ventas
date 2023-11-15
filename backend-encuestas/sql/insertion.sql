e4937261a8c9392becc509798e945832d68130e3
Marcos <marcosugaldemartinez@gmail.com>
2023-11-15 10:57:21 +0100

    fix: se corrige error en archivo sql

--- backend-encuestas/sql/insertion.sql
+++ backend-encuestas/sql/insertion.sql
@@ -1,11 +1,11 @@
 INSERT INTO  users (
     email, username, password, edit_access
 ) VALUES (
-    'user1@gmail.com', 'User 1', crypt('1234', '$2a$10$'), TRUE
+    'user1@gmail.com', 'User 1', '1234', TRUE
 );
 
 INSERT INTO  users (
     email, username, password, edit_access
 ) VALUES (
-    'user2@gmail.com', 'User 2', crypt('1234', '$2a$10$'), FALSE
+    'user2@gmail.com', 'User 2', '1234', FALSE
 )
