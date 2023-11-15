b8ea66aa01eb65df89ececd9d3d36becf7573601
Marcos <marcosugaldemartinez@gmail.com>
2023-11-15 13:29:37 +0100

    feat: se crea tabla para almacenar información de encuestas

--- backend-encuestas/sql/creation.sql
+++ backend-encuestas/sql/creation.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS encuestas;
 DROP TABLE IF EXISTS users;
 
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
@@ -9,3 +10,12 @@ CREATE TABLE IF NOT EXISTS users (
     password TEXT NOT NULL,
     edit_access BOOLEAN DEFAULT FALSE
 );
+
+CREATE TABLE IF NOT EXISTS encuestas (
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    client_dni TEXT NOT NULL UNIQUE,
+    product TEXT NOT NULL,
+    subproduct TEXT NOT NULL,
+    mantenimiento TEXT NOT NULL,
+    estado TEXT NOT NULL
+)
