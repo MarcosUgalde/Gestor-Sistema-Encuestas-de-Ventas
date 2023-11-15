9ae8bfeda6d0dfb3e8d251a8b303a75d97e1d1dd
Marcos <marcosugaldemartinez@gmail.com>
2023-11-15 13:09:53 +0100

    feat: se crea NavBar y se importa en la página principal

---
+++ frontend-encuestas/src/components/NavBar/styles.jsx
@@ -0,0 +1,54 @@
+import { styled } from 'styled-components'
+
+const Nav = styled.nav`
+    display: flex;
+    justify-content: flex-end;
+    font-size: 1.2rem;
+    gap: 1.1em;
+    background-color: #22333b;
+`
+
+const Anchor = styled.a`
+    ${({ isActive }) => `
+        color: ${isActive ? '#7DD181' : '#4DA1A9'};
+        text-decoration: ${!isActive && 'none'};
+        transition: 0.4s
+    `}
+`
+const Userlogout = styled.section`
+    display: flex;
+    flex-direction: column;
+`
+
+const User = styled.span`
+    color: red;
+    cursor: pointer;
+    position: relative;
+`
+
+const Dropdown = styled.ul`
+    position: absolute;
+    background-color: #fff;
+    list-style: none;
+    padding: 0;
+    border-radius: 4px;
+    box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.2);
+    z-index: 10;
+
+    li {
+    cursor: pointer;
+    color: black;
+
+    &:hover {
+        color: red;
+        }
+    }
+`
+
+export default {
+    Nav,
+    Anchor,
+    Userlogout,
+    User,
+    Dropdown
+}
--- frontend-encuestas/src/pages/Main/index.jsx
+++ frontend-encuestas/src/pages/Main/index.jsx
@@ -1,6 +1,9 @@
+import NavBar from "../../components/NavBar"
+
 function Mainpage () {
     return (
         <>
+            <NavBar />
             Hola mundo
         </>
     )
