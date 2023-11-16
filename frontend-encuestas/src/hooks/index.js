737dffbba88c5ec295cb5a7f12c669ffeb2eb04d
Marcos <marcosugaldemartinez@gmail.com>
2023-11-16 13:24:07 +0100

    feat: se crea hook para la edición de encuestas almacenadas

--- frontend-encuestas/src/hooks/index.js
+++ frontend-encuestas/src/hooks/index.js
@@ -2,3 +2,4 @@ export { useUser } from "./useUser";
 export { useLogout } from "./useLogout";
 export { useEncuestas } from "./useEncuestas";
 export { useInsertion } from "./useInsertion";
+export { useUpdate } from "./useUpdate";
---
+++ frontend-encuestas/src/hooks/useUpdate.js
@@ -0,0 +1,17 @@
+import { useMutation } from "react-query";
+import { useLocation } from "wouter";
+import { encuestas } from "../services";
+
+export const useUpdate = () => {
+  const [, setLocation] = useLocation();
+
+  const { mutate: updateEncuesta } = useMutation({
+    mutationFn: encuestas.updateEncuesta,
+    onSuccess: (data) => {
+      if (data.success) setLocation("/");
+      console.info("> Encuesta update completed");
+    },
+  });
+
+  return updateEncuesta;
+};
