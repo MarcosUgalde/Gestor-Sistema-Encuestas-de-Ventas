281389ce2c4588c32e0a07e2a28c3ce7c91dac33
Marcos <marcosugaldemartinez@gmail.com>
2023-11-16 14:48:42 +0100

    feat: se crea modal para eliminar una encuesta

---
+++ frontend-encuestas/src/components/DeleteModal/index.jsx
@@ -0,0 +1,16 @@
+import Styled from './styles' 
+
+function DeleteModal({ onClose, onDelete, isVisible }) {
+    return (
+        <Styled.Modal style={{ display: isVisible ? 'block' : 'none' }}>
+            <h2>Estás a punto de elimianr una encuesta</h2>
+            <p>¿Estás seguro de que quieres eliminar esta encuesta?</p>
+            <Styled.Buttons>
+                <Styled.Delete onClick={onDelete}>Delete</Styled.Delete>
+                <Styled.Cancel onClick={onClose}>Cancel</Styled.Cancel>
+            </Styled.Buttons>
+        </Styled.Modal>
+    )
+}
+
+export default DeleteModal
