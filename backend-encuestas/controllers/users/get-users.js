9a9aa22c1d6198dcdef592bb56a172b68561c022
Marcos <marcosugaldemartinez@gmail.com>
2023-11-16 15:43:10 +0100

    fix: se corrige código para obtener también información de permisos de usuario

--- backend-encuestas/controllers/users/get-users.js
+++ backend-encuestas/controllers/users/get-users.js
@@ -1,11 +1,12 @@
 module.exports = (db) => async (req, res, next) => {
-  const { username, email } = res.locals.user;
+  const { username, email, access } = res.locals.user;
 
   res.status(200).json({
     success: true,
     data: {
       username,
       email,
+      access,
     },
   });
 };
