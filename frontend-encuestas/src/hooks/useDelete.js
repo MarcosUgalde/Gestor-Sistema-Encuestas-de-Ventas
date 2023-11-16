be5a9553f885ddc6b88f4554f7079b09923ceb81
Marcos <marcosugaldemartinez@gmail.com>
2023-11-16 15:09:13 +0100

    fix: se corrige error de código

--- frontend-encuestas/src/hooks/useDelete.js
+++ frontend-encuestas/src/hooks/useDelete.js
@@ -1,12 +1,14 @@
 import { useQueryClient, useMutation } from "react-query";
+import { useLocation } from "wouter";
 import { encuestas } from "../services";
 
 export const useDelete = () => {
   const queryClient = useQueryClient();
+  const [, setLocation] = useLocation();
 
   const deleteMutation = useMutation((id) => encuestas.eliminateEncuesta(id), {
     onSuccess: () => {
-      queryClient.invalidateQueries("encuestas");
+      queryClient.invalidateQueries("encuestas") && setLocation("/");
     },
   });
 
