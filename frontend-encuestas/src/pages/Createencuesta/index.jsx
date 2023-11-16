8a016d34e982e1498163a8d5f0edbb07b95f7603
Marcos <marcosugaldemartinez@gmail.com>
2023-11-16 12:36:13 +0100

    feat: se envían los datos desde el formulario a la base de datos

--- frontend-encuestas/src/pages/Createencuesta/index.jsx
+++ frontend-encuestas/src/pages/Createencuesta/index.jsx
@@ -1,10 +1,12 @@
 import NavBar from "../../components/NavBar"
-import { subproduct, mantenimiento } from '../../utils'
 import { useState } from "react"
+import { useForm } from "react-hook-form"
+import { useInsertion } from "../../hooks"
 
 function Create() {
-console.log(subproduct) 
-console.log(mantenimiento)
+const { register, handleSubmit } = useForm()
+const doInsertion = useInsertion()
+
 const [selectedProduct, setSelectedProduct] = useState('')
 const [showTarifaSubproduct, setShowTarifaSubproduct] = useState(false);
 const [showGasSubproduct, setShowGasSubproduct] = useState(false);
@@ -24,12 +26,12 @@ const handleProductSelect = (selectedValue) => {
     return (
         <>
             <NavBar />
-            <form>
+            <form onSubmit={handleSubmit(doInsertion)}>
                 <h1>Nueva encuesta</h1>
                 <label htmlFor="client_dni">Inserte su DNI</label>
-                <input type="text" name="client_dni" id="client_dni" />
+                <input type="text" name="client_dni" id="client_dni" {...register('client_dni', {required: true})} />
                 <label htmlFor="product">Seleccione un producto</label>
-                <select name="product" id="product" onChange={(e) => handleProductSelect(e.target.value)}>
+                <select name="product" id="product" onChange={(e) => {handleProductSelect(e.target.value);}} {...register('product', {required: true})} >
                     <option value="Luz">LUZ</option>
                     <option value="Gas">GAS</option>
                     <option value="Dual">DUAL</option>
@@ -37,7 +39,7 @@ const handleProductSelect = (selectedValue) => {
                 {showTarifaSubproduct && (
                     <div>
                     <label htmlFor="subproduct">Elija una opción para subproducto LUZ</label>
-                    <select name="subproduct" id="subproduct">
+                    <select name="subproduct" id="subproduct" {...register('subproduct', {required: true})}>
                         <option value="Tarifa plana">TARIFA PLANA</option>
                         <option value="Tarifa por uso">TARIFA POR USO</option>
                     </select>
@@ -47,8 +49,8 @@ const handleProductSelect = (selectedValue) => {
                 {showGasSubproduct && (
                     <div>
                         <label htmlFor="subproduct">Elija una opción para subproducto GAS</label>
-                        <select name="subproduct" id="subproduct">
-                            <option value="Plana">PLENA</option>
+                        <select name="subproduct" id="subproduct" {...register('subproduct', {required: true})}>
+                            <option value="Plena">PLENA</option>
                             <option value="Total">TOTAL</option>
                         </select>
                     </div>
@@ -57,7 +59,7 @@ const handleProductSelect = (selectedValue) => {
                         <div>
                             <label htmlFor="mantenimiento">Mantenimiento</label>
                     
-                                <select name="mantenimiento" id="mantenimiento">
+                                <select name="mantenimiento" id="mantenimiento" {...register('mantenimiento', { required: true })}>
                                     <option value="SÍ">SÍ</option>
                                     <option value="NO">NO</option>
                                 </select>
@@ -66,7 +68,7 @@ const handleProductSelect = (selectedValue) => {
                     {showMantenimientoLuz && (
                         <div>
                             <label htmlFor="mantenimiento">Mantenimientos LUZ</label>
-                                <select name="mantenimiento" id="mantenimiento">
+                                <select name="mantenimiento" id="mantenimiento" {...register('mantenimiento', { required: true })}>
                                     <option value="SÍ">SÍ</option>
                                     <option value="NO">NO</option>
                                 </select>
@@ -75,7 +77,7 @@ const handleProductSelect = (selectedValue) => {
                     {showMantenimientoGas && (
                         <div>
                             <label htmlFor="mantenimiento">Mantenimientos GAS</label>
-                                <select name="mantenimiento" id="mantenimiento">
+                                <select name="mantenimiento" id="mantenimiento" {...register('mantenimiento', { required: true })}>
                                     <option value="SÍ">SÍ</option>
                                     <option value="NO">NO</option>
                                 </select>
@@ -83,11 +85,11 @@ const handleProductSelect = (selectedValue) => {
                     )}
 
                 <label htmlFor="estado">Estado</label>
-                <select name="" id="">
-                    <option value="">VENDIDO</option>
-                    <option value="">EN PROCESO</option>
-                    <option value="">NO VENDIDO</option>
-                    <option value="">NO VÁLIDO</option>
+                <select name="estado" id="estado" {...register('estado', { required: true })}>
+                    <option value="VENDIDO">VENDIDO</option>
+                    <option value="EN PROCESO">EN PROCESO</option>
+                    <option value="NO VENDIDO">NO VENDIDO</option>
+                    <option value="NO VÁLIDO">NO VÁLIDO</option>
                 </select>
                 <input type="submit" />
             </form>
