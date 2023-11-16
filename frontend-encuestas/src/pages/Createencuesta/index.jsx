69248364af34a4bf745b7e26e4ea59c7cb89257b
Marcos <marcosugaldemartinez@gmail.com>
2023-11-16 09:18:05 +0100

    feat: se añaden campos al formulario

--- frontend-encuestas/src/pages/Createencuesta/index.jsx
+++ frontend-encuestas/src/pages/Createencuesta/index.jsx
@@ -4,7 +4,32 @@ function Create() {
     return (
         <>
             <NavBar />
-            Hola mundo
+            <form>
+                <h1>Nueva encuesta</h1>
+                <label htmlFor="client_dni">Inserte su DNI</label>
+                <input type="text" name="client_dni" id="client_dni" />
+                <label htmlFor="product">Seleccione un producto</label>
+                <select name="product" id="product">
+                    <option value="Luz">LUZ</option>
+                    <option value="Gas">GAS</option>
+                    <option value="Dual">DUAL</option>
+                </select>
+                <label htmlFor="subproduct">Elija una opción</label>
+                <select name="subproduct" id="subproduct">
+                    <option value=""></option>
+                    <option value=""></option>
+                    <option value=""></option>
+                </select>
+                <label htmlFor="mantenimiento">Mantenimiento</label>
+                <select name="" id=""></select>
+                <label htmlFor="estado">Estado</label>
+                <select name="" id="">
+                    <option value="">VENDIDO</option>
+                    <option value="">EN PROCESO</option>
+                    <option value="">NO VENDIDO</option>
+                    <option value="">NO VÁLIDO</option>
+                </select>
+            </form>
         </>
     )
 }
