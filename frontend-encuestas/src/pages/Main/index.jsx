290cc4482ab4cce1c57338ed7bafce65f9f2fdc2
Marcos <marcosugaldemartinez@gmail.com>
2023-11-17 11:19:11 +0100

    style: se añaden estilos al panel principal

--- frontend-encuestas/src/pages/Main/index.jsx
+++ frontend-encuestas/src/pages/Main/index.jsx
@@ -17,39 +17,41 @@ function Mainpage () {
         
             <Styled.Body>
                 <NavBar />
-                <h1>Todas las encuestas</h1>
-                <Styled.Table>
-                    <th>DNI de cliente</th>
-                    <th>Producto</th>
-                    <th>Subproducto Gas</th>
-                    <th>Subproducto Luz</th>
-                    <th>Mantenimiento Gas</th>
-                    <th>Mantenimiento Luz</th>
-                    <th>Estado</th>
-                {response?.content?.map((encuesta) => {
-                        return (
-                            <>
-                                <tr>
-                                    <td>
-                                        <Link href={`/update/${encuesta.id}`} key={encuesta.id}>
-                                            <Styled.Anchor>
-                                                {encuesta.client_dni}
-                                            </Styled.Anchor>
-                                        </Link>
-                                    </td>
-                                    <td>{encuesta.product}</td>
-                                    <td>{encuesta.subproduct_gas}</td>
-                                    <td>{encuesta.subproduct_light}</td>
-                                    <td>{encuesta.maintenance_gas}</td>
-                                    <td>{encuesta.maintenance_light}</td>
-                                    <td>{encuesta.estado}</td>
-                                </tr>
-                            </>
-                        )
-                        
-                }
-                    )} 
-                </Styled.Table>
+                <Styled.Main>
+                    <h1>Todas las encuestas</h1>
+                    <Styled.Table>
+                        <Styled.Client>DNI de cliente</Styled.Client>
+                        <Styled.Product>Producto</Styled.Product>
+                        <Styled.Subproductgas>Subproducto Gas</Styled.Subproductgas>
+                        <Styled.Subproductlight>Subproducto Luz</Styled.Subproductlight>
+                        <Styled.Maintenancegas>Mantenimiento Gas</Styled.Maintenancegas>
+                        <Styled.Maintenancelight>Mantenimiento Luz</Styled.Maintenancelight>
+                        <Styled.Estado>Estado</Styled.Estado>
+                    {response?.content?.map((encuesta) => {
+                            return (
+                                <>
+                                    <tr>
+                                        <Styled.Cell>
+                                            <Link href={`/update/${encuesta.id}`} key={encuesta.id}>
+                                                <Styled.Anchor>
+                                                    {encuesta.client_dni}
+                                                </Styled.Anchor>
+                                            </Link>
+                                        </Styled.Cell>
+                                        <Styled.Cell2>{encuesta.product}</Styled.Cell2>
+                                        <Styled.Cell>{encuesta.subproduct_gas ? encuesta.subproduct_gas : <p><i>N/A</i></p>}</Styled.Cell>
+                                        <Styled.Cell2>{encuesta.subproduct_light ? encuesta.subproduct_light : <p><i>N/A</i></p>}</Styled.Cell2>
+                                        <Styled.Cell>{encuesta.maintenance_gas ? encuesta.maintenance_gas : <p><i>N/A</i></p>}</Styled.Cell>
+                                        <Styled.Cell2>{encuesta.maintenance_light ? encuesta.maintenance_light : <p><i>N/A</i></p>}</Styled.Cell2>
+                                        <Styled.Cell>{encuesta.estado}</Styled.Cell>
+                                    </tr>
+                                </>
+                            )
+                            
+                    }
+                        )} 
+                    </Styled.Table>
+                </Styled.Main>
             </Styled.Body>
         
     )
--- frontend-encuestas/src/pages/Main/styles.jsx
+++ frontend-encuestas/src/pages/Main/styles.jsx
@@ -4,9 +4,60 @@ const Body = styled.section`
     background-color: #C9FFE2;
     min-height: 98vh;
 `
+const Main = styled.main`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+
+const Client = styled.th`
+    width: 150px;
+    height: 2.5em;
+    background-color: #7DD181;
+    padding-top: 1.2em;
+`
+
+const Product = styled.th`
+    width: 100px;
+    background-color: #96E8BC;
+`
+const Subproductgas = styled.th`
+    width: 130px;
+    background-color: #7DD181;
+`
+
+const Subproductlight = styled.th`
+    background-color: #96E8BC;
+`
+
+const Maintenancegas = styled.th`
+    background-color: #7DD181;
+`
+
+const Maintenancelight = styled.th`
+    background-color: #96E8BC;
+`
+
+const Estado = styled.th`
+    width: 150px;
+    height: 2.5em;
+    background-color: #7DD181;
+    padding-top: 1.2em;
+`
 
 const Table = styled.section`
     box-shadow: 1px 12px 8px -4px rgba(0,0,0,0.75);
+    width: fit-content;
+`
+const Cell = styled.td`
+    height: 2.0em;
+    background-color: #7DD181;
+    padding: 10px;
+`
+
+const Cell2 = styled.td`
+    background-color: #96E8BC;
+    padding: 10px;
 `
 
 const Anchor = styled.a`
@@ -18,6 +69,16 @@ const Anchor = styled.a`
 
 export default {
     Body,
+    Main,
     Table,
+    Client,
+    Product,
+    Subproductgas,
+    Subproductlight,
+    Maintenancegas,
+    Maintenancelight,
+    Estado,
+    Cell,
+    Cell2,
     Anchor
 }
